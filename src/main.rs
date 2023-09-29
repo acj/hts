3ea3f37b662a95eb10bd53bdcd7098721cb0198c
Adam Jensen <adam@acj.sh>
2023-09-28 22:10:10 -0400

    Decompose main into functions that are testable

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use chrono::DateTime;
 use clap::Parser;
 use colored::Colorize;
 use duration_string::DurationString;
-use std::io::BufRead;
+use std::io::{BufRead, BufReader};
 
 /// Highlight lines of program output based on the latency between them
 #[derive(Parser)]
@@ -36,12 +36,65 @@ fn main() {
     let min_latency = chrono::Duration::from_std(cli.min_latency.into()).unwrap();
 
     let start_time = chrono::Utc::now();
-    let lines: Vec<AnnotatedLine> = std::io::stdin()
-        .lock()
+    let lines = lines_from_stream(BufReader::new(std::io::stdin()), !cli.no_echo);
+
+    if lines.is_empty() {
+        return;
+    }
+
+    let max_inter_line_latency = max_inter_line_latency(&lines);
+    let latency_display_width = latency_display_width(max_inter_line_latency, &cli.latency_unit);
+
+    lines
+        .iter()
+        .enumerate()
+        .map(|(index, line)| {
+            let since_last_line = if index == 0 {
+                lines.first().unwrap().timestamp - start_time
+            } else {
+                line.timestamp - lines[index - 1].timestamp
+            };
+            let red_intensity = if since_last_line > min_latency {
+                let num = since_last_line.num_nanoseconds().unwrap() as f64;
+                let denom = max_inter_line_latency.num_nanoseconds().unwrap() as f64;
+                (255.0 * num / denom) as u8
+            } else {
+                0
+            };
+            let debug = if cli.debug {
+                debug_preamble(
+                    since_last_line,
+                    max_inter_line_latency,
+                    red_intensity,
+                    line.timestamp,
+                )
+            } else {
+                "".to_string()
+            };
+            format!(
+                "{} {:>lat_width$}{} {} {}",
+                "  ".on_truecolor(red_intensity, 0, 0),
+                duration_as_unit(since_last_line, &cli.latency_unit),
+                cli.latency_unit,
+                debug,
+                line.line,
+                lat_width = latency_display_width
+            )
+        })
+        .for_each(|line| {
+            println!("{}", line);
+        });
+}
+
+fn lines_from_stream<R>(stream: R, echo: bool) -> Vec<AnnotatedLine>
+where
+    R: BufRead,
+{
+    let lines: Vec<AnnotatedLine> = stream
         .lines()
         .filter_map(|x| x.ok())
         .inspect(|line| {
-            if !cli.no_echo {
+            if echo {
                 println!("{}", line);
             }
         })
@@ -50,11 +103,40 @@ fn main() {
             timestamp: chrono::Utc::now(),
         })
         .collect();
+    lines
+}
 
-    if lines.is_empty() {
-        return;
-    }
+fn debug_preamble(
+    since_last_line: chrono::Duration,
+    max_inter_line_latency: chrono::Duration,
+    red_intensity: u8,
+    timestamp: DateTime<chrono::Utc>,
+) -> String {
+    let max_lat_indicator = if since_last_line == max_inter_line_latency {
+        "MAX".to_string()
+    } else {
+        "".to_string()
+    };
+    format!(
+        "{} | R{} {} | ",
+        max_lat_indicator, red_intensity, timestamp
+    )
+}
 
+fn latency_display_width(max_inter_line_latency: chrono::Duration, latency_unit: &str) -> usize {
+    let latency_display_width: usize = if max_inter_line_latency > chrono::Duration::zero() {
+        let d: usize = duration_as_unit(max_inter_line_latency, latency_unit)
+            .ilog10()
+            .try_into()
+            .unwrap();
+        d + 1
+    } else {
+        1
+    };
+    latency_display_width
+}
+
+fn max_inter_line_latency(lines: &Vec<AnnotatedLine>) -> chrono::Duration {
     let mut max_inter_line_latency = chrono::Duration::milliseconds(1);
     for (index, line) in lines.iter().enumerate() {
         if index == 0 {
@@ -65,53 +147,7 @@ fn main() {
             max_inter_line_latency = since_last_line;
         }
     }
-
-    let latency_display_width: usize = if max_inter_line_latency > chrono::Duration::zero() {
-        let d: usize = duration_as_unit(max_inter_line_latency, &cli.latency_unit)
-            .ilog10()
-            .try_into()
-            .unwrap();
-        d + 1
-    } else {
-        1
-    };
-
-    for (index, line) in lines.iter().enumerate() {
-        let since_last_line = if index == 0 {
-            lines.first().unwrap().timestamp - start_time
-        } else {
-            line.timestamp - lines[index - 1].timestamp
-        };
-        let red_intensity = if since_last_line > min_latency {
-            let num = since_last_line.num_nanoseconds().unwrap() as f64;
-            let denom = max_inter_line_latency.num_nanoseconds().unwrap() as f64;
-            (255.0 * num / denom) as u8
-        } else {
-            0
-        };
-        let debug = if cli.debug {
-            let max_lat_indicator = if since_last_line == max_inter_line_latency {
-                "MAX".to_string()
-            } else {
-                "".to_string()
-            };
-            format!(
-                "{} | R{} {} | ",
-                max_lat_indicator, red_intensity, line.timestamp
-            )
-        } else {
-            "".to_string()
-        };
-        println!(
-            "{} {:>lat_width$}{} {} {}",
-            "  ".on_truecolor(red_intensity, 0, 0),
-            duration_as_unit(since_last_line, &cli.latency_unit),
-            cli.latency_unit,
-            debug,
-            line.line,
-            lat_width = latency_display_width
-        );
-    }
+    max_inter_line_latency
 }
 
 fn duration_as_unit(duration: chrono::Duration, unit: &str) -> u64 {
